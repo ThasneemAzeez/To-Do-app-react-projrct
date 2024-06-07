f81c0c47a77fac36806985608aacca6967bb46ce
ThasneemAzeez <thasneemazeez2001@gmail.com>
2024-06-08 02:54:25 +0530

    not completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
+import Search from './components/Search';
+import Delete from './components/Delete';
 
 function App() {
   return (
     <div >
       <Home/>
+      <Search/>
+      <Delete/>
       
     </div>
   );
---
+++ src/components/Search.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Search = () => {
+  return (
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <h1><center>Search</center></h1>
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-control">Work no</label>
+                            <input type="text" className="form-control" />
+                            <button className="btn btn-success">Search</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Search
